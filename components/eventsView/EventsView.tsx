86bb5cd3f4b939fc960bb15c6b65854ca7c020ea
FERAND Cyrille <c.ferand@smallable.com>
2024-01-15 01:11:19 +0100

    fix build

--- components/eventsView/EventsView.tsx
+++ components/eventsView/EventsView.tsx
@@ -5,12 +5,35 @@ import Button from "@/components/form/button/button";
 import EventInfos from "@/components/event/event";
 
 interface EventsViewProps {
-    id: string
-    email: string
-    firstname: string
+    events: Event[]
 }
 
-const EventsView: React.FC = async ({events}) => {
+interface Event {
+    id: string
+    title: string
+    location: string
+    date: string
+    category: string
+    createdAt: string
+    participantMax: number
+    guests: User[]
+    host: User
+}
+
+interface User {
+    id: string
+    email: string
+    sex: string
+    firstname: string
+    lastname: string
+    bio: string
+    birthday: string
+    city: string
+    pictureUrl: string| null
+    createdAt: string
+}
+
+const EventsView: React.FC<EventsViewProps> = async ({events}) => {
     return (<>
         <div className={s.container}>
             <SideMenu/>
